ed3441c71e26178cddb9537a830da3939239871f
Ramya Ashrit <ramyaashrit@gmail.com>
2022-05-05 16:34:31 +0530

    subtraction function added

--- Arithmetic_operations.py
+++ Arithmetic_operations.py
@@ -3,3 +3,8 @@ class aithmetic_operations:
     def addition(self,a,b):#function for addition
         sum=a+b
         print(sum)
+
+    def sub(self,a,b):#function for subtraction
+        diff=a-b
+        print(diff)
+    
